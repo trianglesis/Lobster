e2e263c9415b7fa1490f53eb91b57a0ba7f07463
Oleksandr Danylchenko <o.danylchenko@globallogic.com>
2019-11-02 13:24:58 +0200

    Remove old code

--- octo_adm/urls.py
+++ octo_adm/urls.py
@@ -4,7 +4,7 @@ Keep here admin part of Octopus site
 
 from rest_framework.routers import DefaultRouter
 from django.conf.urls import url, include
-from octo_adm.views import AdminFunctions, CeleryInteract, TaskOperationsREST, AdminOperationsREST, ListAllAddmVmREST
+from octo_adm.views import *
 
 router = DefaultRouter()
 router.register(r'addm_sets', ListAllAddmVmREST)
@@ -13,7 +13,7 @@ urlpatterns = [
     # REST model views
     url(r'^', include(router.urls)),
     # Admin
-    url(r'^addm_workbench/', AdminFunctions.addm_workbench_widgets, name='addm_workbench'),
+    url(r'^old_addm_workbench/', AdminFunctions.addm_workbench_widgets, name='old_addm_workbench'),
 
     # ADDM service
     url(r'^addm_buttons_page/', AdminFunctions.addm_buttons_page, name='addm_buttons_page'),
@@ -28,6 +28,9 @@ urlpatterns = [
     url(r'^celery_service_restart/', CeleryInteract.celery_service_restart, name='celery_service_restart'),
 
     # NEW:
+    url(r'^admin_workbench/', AdminWorkbench.as_view(), name='admin_workbench'),
+    url(r'^celery_workbench/', CeleryWorkbench.as_view(), name='celery_workbench'),
+    url(r'^addm_workbench/', AddmWorkbench.as_view(), name='addm_workbench'),
     # REST Support for ADMIN functions and tasks:
     url(r'^task_operation/', TaskOperationsREST.as_view(), name='task_operations'),
     url(r'^admin_operations/', AdminOperationsREST.as_view(), name='admin_operations'),
