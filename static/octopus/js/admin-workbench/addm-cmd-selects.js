81f8ad4bd86c0ddda4f526231d4e5b931c1d7319
Oleksandr Danylchenko <o.danylchenko@globallogic.com>
2019-12-03 21:05:42 +0200

    Small UI improvements: + ADDM CMD multiple selection

--- static/octopus/js/admin-workbench/addm-cmd-selects.js
+++ static/octopus/js/admin-workbench/addm-cmd-selects.js
@@ -1,15 +1,55 @@
-function changeFunc() {
-    let sortedKeys = [];
+$(document).ready(function () {
+    $('#addmCMDRun').on('hidden.bs.modal', function (event) {
+        $('#addmCMDRun').modal('hide');
+    });
+});
+
+function addmCMDkeysSelected() {
+    let selectedCMDs = [];
     let selectBox = document.getElementById("addmCMDSelect");
     let allOptions = selectBox.options;
     for (let selected of allOptions) {
         if (selected.selected) {
-            if (sortedKeys.includes(selected.value)){
+            if (selectedCMDs.includes(selected.value)) {
                 console.log('Already selected')
             } else {
-                sortedKeys.push(selected.value);
+                selectedCMDs.push(selected.value);
             }
         }
     }
-    console.log(sortedKeys);
+    console.log(selectedCMDs);
+    return selectedCMDs;
 }
+
+function addmGroupsSelected() {
+    let checkedADDMs = [];
+    let checkBoxes = document.getElementsByClassName("form-check-input");
+
+    for (let checkbox of checkBoxes) {
+        if (checkbox.checked) {
+            if (checkedADDMs.includes(checkbox.value)) {
+                console.log('Already selected')
+            } else {
+                checkedADDMs.push(checkbox.value);
+            }
+        }
+    }
+    console.log(checkedADDMs);
+    return checkedADDMs;
+}
+
+$(document).ready(function () {
+    let modal = document.getElementById("addmCMDRun");
+    let runAddmCMD = document.getElementById("runAddmCMD");
+
+    let checkedADDMs = '';
+    let selectedCMDs = '';
+
+    runAddmCMD.addEventListener("click", function () {
+        checkedADDMs = addmGroupsSelected();
+        selectedCMDs = addmCMDkeysSelected();
+        $('#addmCMDRun').modal('hide');
+        console.log(`${checkedADDMs} ${selectedCMDs}`);
+    });
+
+});
