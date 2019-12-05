8be794f79ba70dc51c838c08b96a20e22e53e59f
trianglesis <sanekm2010@gmail.com>
2019-12-05 18:14:25 +0200

    Small UI improvements: + ADDM CMD multiple selection + ADDM CMD multiple REST run + ADDM CMD service button select single ADDM by host + Fix multiple listeners for one target + ADDM CMD run for all {{ all_addm_groups|join:"," }} + Fix addm branch select + Fix addm single CMD select, multiple instances, diff toasts + Single ADDM run clean, sync test/utils + Fix toast ID based on keys, addm groups/hosts

--- static/octopus/js/test-cases-tables-methods.js
+++ static/octopus/js/test-cases-tables-methods.js
@@ -731,12 +731,19 @@ function getToastDraft(btnDataSet) {
     let toastBase = toastDraft.children[0].cloneNode(true);  // Toast object
     toastBase.setAttribute('data-delay', 30000); // 30 sec. Wait to task mod
     // Assign new toast copy a value ID based on item ID (case id or case unique attrs)
+
     if (btnDataSet.addm_group) {
         toastBase.id = `${btnDataSet.operation_key}-${btnDataSet.addm_group}`;
+
     } else if (btnDataSet.addm_host && btnDataSet.command_key) {
         toastBase.id = `${btnDataSet.addm_host}-${btnDataSet.command_key}`;
+
+    } else if (btnDataSet.addm_host && btnDataSet.operation_key) {
+        toastBase.id = `${btnDataSet.addm_host}-${btnDataSet.operation_key}`;
+
     } else if (btnDataSet.addm_group && btnDataSet.command_key) {
         toastBase.id = `${btnDataSet.addm_group}-${btnDataSet.command_key}`;
+
     } else {
         toastBase.id = `${btnDataSet.operation_key}`;
     }
