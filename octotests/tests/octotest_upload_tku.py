12ece54409464299b4a91e6f76230557f4f6235e
trianglesis <sanekm2010@gmail.com>
2020-01-10 18:59:38 +0200

    Upload test routines ~ better logging ~ better mailing ~ make stubs for vcenter ~ changing ADDM prep ~ Install TKU single packs: product content, tideway devices
    models.py UploadTestsNew
    ~ add str method
    octo_tku_upload test_executor.py
    ~ wrapper debug massages, place to mail send call
    
    modal_wgetRefresh.html
    ~ fix WGET Refresh btn

--- octotests/tests/octotest_upload_tku.py
+++ octotests/tests/octotest_upload_tku.py
@@ -18,57 +18,59 @@ class OctoTestCaseUpload(octo_tests.OctoTestCase):
         # self.user_and_mail('Danylcha', "Dan@bmc.com")
 
     def test001_product_content_update_tkn_main(self):
-        self.debug = True
         self.silent = True
         self.tku_wget = False
         self.fake_run = False
         self.test_mode = 'tideway_content'
-        package_type = self.select_latest_continuous(tkn_branch='tkn_main')
         self.package_detail = 'TKU-Product-Content'
+        package_type = self.select_latest_continuous(tkn_branch='tkn_main')
         self.package_types = [package_type]
-        self.addm_group = 'golf'
-        # self.addm_set = self.addm_set.filter(
-        #     addm_group__in=['alpha', 'golf'],
-        #     disables__isnull=True).values().order_by('addm_group')
+        self.addm_set = self.addm_set.filter(
+            addm_group__in=['alpha', 'beta', 'echo' 'golf'],
+            addm_name__in=['custard_cream', 'double_decker'],  # Skip FF till tpl 12
+            disables__isnull=True).values().order_by('addm_group')
         self.run_case()
 
     def test002_product_content_update_tkn_ship(self):
-        self.debug = True
         self.silent = True
         self.tku_wget = False
-        self.fake_run = True
+        self.fake_run = False
         self.test_mode = 'tideway_content'
-        package_type = self.select_latest_continuous(tkn_branch='tkn_ship')
         self.package_detail = 'TKU-Product-Content'
+        package_type = self.select_latest_continuous(tkn_branch='tkn_ship')
         self.package_types = [package_type]
-        self.addm_group = 'golf'
+        self.addm_set = self.addm_set.filter(
+            addm_group__in=['charlie', 'delta', 'foxtrot'],
+            addm_name__in=['custard_cream', 'double_decker'],  # Skip FF till tpl 12
+            disables__isnull=True).values().order_by('addm_group')
         self.run_case()
 
     def test003_tideway_devices_update_tkn_main(self):
-        self.debug = True
         self.silent = True
         self.tku_wget = False
         self.fake_run = False
         self.test_mode = 'tideway_devices'
-        package_type = self.select_latest_continuous(tkn_branch='tkn_main')
         self.package_detail = 'tideway-devices'
+        package_type = self.select_latest_continuous(tkn_branch='tkn_main')
         self.package_types = [package_type]
-        self.addm_group = 'golf'
-        # self.addm_set = self.addm_set.filter(
-        #     addm_group__in=['alpha', 'golf'],
-        #     disables__isnull=True).values().order_by('addm_group')
+        self.addm_set = self.addm_set.filter(
+            addm_group__in=['alpha', 'beta', 'echo' 'golf'],
+            addm_name__in=['custard_cream', 'double_decker'],  # Skip FF till tpl 12
+            disables__isnull=True).values().order_by('addm_group')
         self.run_case()
 
     def test004_tideway_devices_update_tkn_ship(self):
-        self.debug = True
         self.silent = True
         self.tku_wget = False
-        self.fake_run = True
+        self.fake_run = False
         self.test_mode = 'tideway_devices'
-        package_type = self.select_latest_continuous(tkn_branch='tkn_ship')
         self.package_detail = 'tideway-devices'
+        package_type = self.select_latest_continuous(tkn_branch='tkn_ship')
         self.package_types = [package_type]
-        self.addm_group = 'golf'
+        self.addm_set = self.addm_set.filter(
+            addm_group__in=['charlie', 'delta', 'foxtrot'],
+            addm_name__in=['custard_cream', 'double_decker'],  # Skip FF till tpl 12
+            disables__isnull=True).values().order_by('addm_group')
         self.run_case()
 
     def test005_release_ga_upgrade(self):
