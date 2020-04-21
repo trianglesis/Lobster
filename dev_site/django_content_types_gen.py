cd0618df81351693b7a2b9c91330e6cacf9d8010
Oleksandr Danylchenko <o.danylchenko@globallogic.com>
2020-04-21 18:10:08 +0300

    django_content_types_gen.py - generate content types - change permissions and AUTH right from scratch

--- dev_site/django_content_types_gen.py
+++ dev_site/django_content_types_gen.py
@@ -1,37 +1,28 @@
 import django
 django.setup()
 # https://medium.com/@gauravtoshniwal/how-to-create-content-types-and-permissions-for-already-created-tables-89a3647ff720
-from django.apps.registry import apps
-
-apps_all = apps.get_app_configs()
-print(apps_all)
-for app in apps_all:
-    print(f'Generating for: {app.name}: {app.label}')
-
-    app_config = apps.get_app_config(app.label)
-
-    # To create Content Types
-    from django.contrib.contenttypes.management import create_contenttypes
-    create_contenttypes(app_config)
-
-    # To create Permissions
-    from django.contrib.auth.management import create_permissions
-    create_permissions(app_config)
-
-
-# # https://gist.github.com/bmihelac/3010093
-# # changing verbose name of model does not change ContentType name
-# # this script will loop through all ContentType objects and refresh names
+# from django.apps.registry import apps
 #
-# # optional activate translation
-# from django.utils import translation
-# translation.activate('en')
+# apps_all = apps.get_app_configs()
+# print(apps_all)
+# for app in apps_all:
+#     print(f'Generating for: {app.name}: {app.label}')
 #
-# from django.contrib.contenttypes.models import ContentType
-# content_types = ContentType.objects.all()
+#     app_config = apps.get_app_config(app.label)
 #
-# for ct in content_types:
-#     model = ct.model_class()
-#     if model:
-#         ct.name = model._meta.verbose_name
-#         ct.save()
+#     # To create Content Types
+#     from django.contrib.contenttypes.management import create_contenttypes
+#     create_contenttypes(app_config)
+#
+#     # To create Permissions
+#     from django.contrib.auth.management import create_permissions
+#     create_permissions(app_config)
+
+
+# Add users to new groups:
+from django.contrib.auth.models import Group, User
+
+g = Group.objects.get(name='Read and execute')
+users = User.objects.all()
+for u in users:
+    g.user_set.add(u)
