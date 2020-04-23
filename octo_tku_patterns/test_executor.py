93a90d05c716ab1c1f367ec5f5e6dc8144365aa3
Oleksandr Danylchenko <o.danylchenko@globallogic.com>
2020-04-23 18:14:58 +0300

    Options add modern_addm value and use for py3 in test exec

--- octo_tku_patterns/test_executor.py
+++ octo_tku_patterns/test_executor.py
@@ -16,6 +16,7 @@ from octo import settings
 from octo.helpers.tasks_helpers import exception
 from octo_tku_patterns.models import TestLast, TestHistory
 from run_core.addm_operations import ADDMOperations
+from run_core.models import Options
 
 log = logging.getLogger("octo.octologger")
 
@@ -135,7 +136,8 @@ class TestExecutor:
         test_py_t = test_item.get('test_py_path_template', False)
         test_time_weight = test_item.get('test_time_weight', '')
 
-        modern_addms = ['fishfinger']
+        modern_addms = Options.objects.get(option_key__exact='modern_addm').replace(' ', '').split(',')
+
         test_info = f" {test_py_t} | '{addm_item['addm_name']}' v'{addm_item['addm_v_int']}' " \
                     f"{addm_item['addm_ip']} - {addm_item['addm_host']} {addm_item['addm_group']}"
 
