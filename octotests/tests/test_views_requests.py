371be683e4c52d89c293101fb7a6b1cac85aef7f
Oleksandr Danylchenko <o.danylchenko@globallogic.com>
2020-07-07 17:48:32 +0300

    Introducing cache for Octopus by django-cachealot and debug_toolbar for local server run. Making cache stable Adding cache-pre as test unit

--- octotests/tests/test_views_requests.py
+++ octotests/tests/test_views_requests.py
@@ -8,20 +8,18 @@ class SimpleTest(unittest.TestCase):
             SERVER_NAME='127.0.0.1',
             HTTP_USER_AGENT='Mozilla/5.0',
         )
-
-    def test_login(self):
         login = self.client.login(username='test', password='rQBzX5SveIokyF0uci5A')
         self.assertTrue(login)
 
-    def test_main_page(self):
+    def test002_main_page(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
-    def test_addm_digest(self):
+    def test003_addm_digest(self):
         response = self.client.get('/octo_tku_patterns/addm_digest/')
         self.assertEqual(response.status_code, 200)
 
-    def test_tests_last(self):
+    def test004_tests_last(self):
         # Non pass
         response = self.client.get('/octo_tku_patterns/tests_last/', {'tkn_branch': 'tkn_main', 'tst_status': 'notpass'})
         self.assertEqual(response.status_code, 200)
@@ -39,7 +37,7 @@ class SimpleTest(unittest.TestCase):
         response = self.client.get('/octo_tku_patterns/tests_last/', {'test_type': 'product_content'})
         self.assertEqual(response.status_code, 200)
 
-    def test_test_history_digest_today(self):
+    def test005_test_history_digest_today(self):
         response = self.client.get('/octo_tku_patterns/test_history_digest_today/', {'tkn_branch': 'tkn_main', 'tst_status': 'notpass'})
         self.assertEqual(response.status_code, 200)
         response = self.client.get('/octo_tku_patterns/test_history_digest_today/', {'tkn_branch': 'tkn_ship', 'tst_status': 'notpass'})
@@ -47,15 +45,15 @@ class SimpleTest(unittest.TestCase):
         response = self.client.get('/octo_tku_patterns/test_history_digest_today/', {'tst_status': 'notpass'})
         self.assertEqual(response.status_code, 200)
 
-    def test_tku_workbench(self):
+    def test008_tku_workbench(self):
         response = self.client.get('/octo_tku_upload/tku_workbench/')
         self.assertEqual(response.status_code, 200)
 
-    def test_upload_today(self):
+    def test007_upload_today(self):
         response = self.client.get('/octo_tku_upload/upload_today/')
         self.assertEqual(response.status_code, 200)
 
-    def test_test_cases(self):
+    def test008_test_cases(self):
         response = self.client.get('/octo_tku_patterns/test_cases/')
         self.assertEqual(response.status_code, 200)
         response = self.client.get('/octo_tku_patterns/test_cases/', {'tkn_branch': 'tkn_main',})
