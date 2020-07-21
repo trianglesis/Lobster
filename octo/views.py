b35e5ff8f5e10ba45f00bf2dca7b57fe14b8d26f
Oleksandr Danylchenko <o.danylchenko@globallogic.com>
2020-07-21 12:53:38 +0300

    Main page 5 min cache

--- octo/views.py
+++ octo/views.py
@@ -35,6 +35,7 @@ import logging
 log = logging.getLogger("octo.octologger")
 
 
+@method_decorator(cache_control(max_age=60 * 5), name='dispatch')
 class MainPage(TemplateView):
     template_name = 'main/mainpage_widgets.html'
     context_object_name = 'objects'
