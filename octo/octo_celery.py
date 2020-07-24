7f834ebe9c646aa7725b24aaeb7a15a65619f4f3
Oleksandr Danylchenko <o.danylchenko@globallogic.com>
2020-07-24 11:28:00 +0300

    CELERY Tuning

--- octo/octo_celery.py
+++ octo/octo_celery.py
@@ -69,7 +69,7 @@ app.conf.update(
     # http://docs.celeryproject.org/en/latest/userguide/configuration.html#database-url-examples
     # https://docs.celeryproject.org/en/latest/getting-started/first-steps-with-celery.html#keeping-results
     # 1406, "Data too long for column 'result' at row 1" - it's not so important to keep it in DB
-    result_backend=result_backend,
+    # result_backend=result_backend,
     database_engine_options={'pool_timeout': 90},
 
     # http://docs.celeryproject.org/en/latest/userguide/configuration.html#beat-scheduler
