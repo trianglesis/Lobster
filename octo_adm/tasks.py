5890303a88985c8b0e0b22e49e4adf6694235462
Oleksandr Danylchenko <o.danylchenko@globallogic.com>
2020-08-11 16:33:40 +0300

    Task for VM threaded operations

--- octo_adm/tasks.py
+++ octo_adm/tasks.py
@@ -13,9 +13,12 @@ from __future__ import absolute_import, unicode_literals
 
 import logging
 
+from octo.helpers.tasks_run import Runner
 from octo.helpers.tasks_helpers import exception
+
 from octo.octo_celery import app
 from run_core.addm_operations import ADDMStaticOperations
+from run_core.models import AddmDev
 from run_core.vcenter_operations import VCenterOperations
 
 log = logging.getLogger("octo.octologger")
@@ -71,3 +74,29 @@ class TaskVMService:
     def t_vm_operation_thread(t_tag, **kwargs):
         log.info(f'Running {t_tag}')
         return VCenterOperations().threaded_operations(**kwargs)
+
+    @staticmethod
+    @app.task(soft_time_limit=MIN_40, task_time_limit=HOURS_1)
+    @exception
+    def t_vm_operation_addm_groups(t_tag, **kwargs):
+        fake_run = kwargs.get('fake_run', False)
+        addm_groups = kwargs.get('addm_groups', '')
+        operation_k = kwargs.get('operation_k', 'vm_power_off')
+        log.info(f'Running {t_tag}')
+
+        if isinstance(addm_groups, str):
+            addm_groups = addm_groups.split(',')
+        elif isinstance(addm_groups, list):
+            pass
+        else:
+            log.error("Addm group should be a list or str!")
+            return False
+
+        for addm_group in addm_groups:
+            addm_set = AddmDev.objects.filter(addm_group__exact=addm_group)
+            Runner.fire_t(TaskVMService.t_vm_operation_thread,
+                          fake_run=fake_run,
+                          t_queue=f"{addm_group}@tentacle.dq2",
+                          t_args=[f"TaskVMService;task=t_vm_power_off_addm_groups;operation_k={operation_k}"],
+                          t_kwargs=dict(addm_set=addm_set, operation_k=operation_k),
+                          t_routing_key=f"{addm_group}.TaskVMService.t_vm_operation_thread.{operation_k}")
