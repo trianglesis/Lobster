38d5bbbc8a6e1428b26e709a598f01cd9ad55b60
trianglesis <it@trianglesis.org.ua>
2021-01-13 13:32:16 +0200

    REST Token auth add

--- octo/settings.py
+++ octo/settings.py
@@ -182,6 +182,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
